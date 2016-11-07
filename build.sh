ce48b43ac46726e60bf9706e36e4e07fd152fb30
huangxiufeng <hxfjsw@qq.com>
2016-11-07 16:15:45 +0800

    添加文档

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 rm -f ./bin/GameFramework
-go build -o ./bin/GameFramework ./src/main.go ./src/room.go ./src/server.go ./src/http.go
+go build -o ./bin/GameFramework ./src/main.go ./src/js_vm.go ./src/room.go ./src/server.go ./src/http.go ./src/redis.go
 ./bin/GameFramework
---
+++ src/js_vm.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"github.com/robertkrimen/otto"
+	"strconv"
+	"time"
+	"os"
+	"io/ioutil"
+)
+
+func init_js() {
+	log.Info("restart js vm")
+	main_js := ""
+
+	if data, err := ioutil.ReadFile("./scripts/main.js"); err != nil {
+		log.Info("not find main.js")
+		os.Exit(-1);
+	} else {
+		main_js = string(data);
+	}
+
+	vm = otto.New()
+	vm.Set("timer_tick", func(call otto.FunctionCall) otto.Value {
+		ttl, _ := strconv.Atoi(call.Argument(0).String())
+		fun := call.Argument(1).String()
+		go timer_tick(time.Duration(ttl), fun);
+		return otto.Value{}
+	})
+	vm.Set("timer_after", func(call otto.FunctionCall) otto.Value {
+		ttl, _ := strconv.Atoi(call.Argument(0).String())
+		fun := call.Argument(1).String()
+		go timer_after(time.Duration(ttl), fun);
+		return otto.Value{}
+	})
+	vm.Set("log", func(call otto.FunctionCall) otto.Value {
+		msg := call.Argument(0).String()
+		log.Info(msg)
+		return otto.Value{}
+	})
+	vm.Set("exit", func(call otto.FunctionCall) otto.Value {
+		os.Exit(0)
+		return otto.Value{}
+	})
+	vm.Set("sendToFd", func(call otto.FunctionCall) otto.Value {
+		fd, _ := strconv.Atoi(call.Argument(0).String())
+		msg := call.Argument(0).String()
+
+		log.Info("SendTo:", fd, msg)
+		hall.SendToFd(fd, msg)
+
+		return otto.Value{}
+	})
+	vm.Set("fdToIp", func(call otto.FunctionCall) otto.Value {
+		msg := call.Argument(0).String()
+		log.Info(msg)
+		return otto.Value{}
+	})
+	if _, err := vm.Run(main_js); err != nil {
+		log.Error(err.Error())
+	};
+}
